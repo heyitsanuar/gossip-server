dc4a2a86a1be3b8b77a18b9951849d4b8dc565ff
Anuar <anuar.jimenez@itexico.com>
2020-04-10 18:46:56 -0500

    Add category routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { ArticleRoutes } from "../entities/article/article.routes";
 import { AuthorRoutes } from "../entities/author/author.routes";
+import { CategoryRoutes } from "../entities/category/category.routes";
 import { SongRoutes } from "../entities/song/song.routes";
 
 export const AppRoutes = Router();
 
 AppRoutes.use("/article", ArticleRoutes);
 AppRoutes.use("/author", AuthorRoutes);
+AppRoutes.use("/category", CategoryRoutes);
 AppRoutes.use("/song", SongRoutes);
