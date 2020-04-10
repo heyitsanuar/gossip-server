d18a5a01ec2a060387f21588fdbaa4b808f11a78
Anuar <anuar.jimenez@itexico.com>
2020-04-10 18:10:16 -0500

    Add cors

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 export const AppController: Application = express();
 
 AppController.use(bodyParser.json());
 AppController.use(bodyParser.urlencoded({ extended: true }));
+
+AppController.use(cors());
 
 // Handling 404 requests
 AppController.use((req: Request, res: Response) => {
